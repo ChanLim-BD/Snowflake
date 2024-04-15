d0a8a9f0be829ec53428e4fe8a38dc7ebf95e82d
chan <linsay070@gmail.com>
2024-04-15 16:24:05 +0900

    .

--- Scripts/DynamicTableHistory.sql
+++ Scripts/DynamicTableHistory.sql
@@ -17,10 +17,11 @@ USE SCHEMA HDHS_PD;
 
 SELECT *
   FROM TABLE(information_schema.DYNAMIC_TABLE_REFRESH_HISTORY (
-      NAME_PREFIX => 'HYUNDAI_DB.HDHS_PD.IM_SLITM_MST'
+      RESULT_LIMIT => 100
     , ERROR_ONLY => FALSE
   )
-);
+)
+;
 
 ------------------------------------------------------------------------------------
 --■■■■■■■■■■■■■ 1.1. DYNAMIC_TABLE_REFRESH_HISTORY로 새로고침 걸린 시간 계산 ■■■■■■■■■■■■■■
@@ -30,31 +31,37 @@ SELECT *
 -- 현재 시간 기준 {}시간 전 부터 검색
 SELECT 
     NAME 
-  , STATE 
+  , STATE
+  , STATE_MESSAGE
   , REFRESH_START_TIME 
   , REFRESH_END_TIME 
   , TO_TIME(TO_TIMESTAMP(TIMESTAMPDIFF(second , REFRESH_START_TIME , REFRESH_END_TIME))) AS REFRESH_DURATION
 FROM 
   TABLE(information_schema.DYNAMIC_TABLE_REFRESH_HISTORY (
-      DATA_TIMESTAMP_START => DATEADD(HOUR, -1, CURRENT_TIMESTAMP()) 
+      DATA_TIMESTAMP_START => DATEADD(HOUR, -4, CURRENT_TIMESTAMP()) 
      , ERROR_ONLY => FALSE
   )
-);
-
+)
+ORDER BY  NAME 
+         ,REFRESH_DURATION DESC;
 
 -- 특정 시간부터 검색
 SELECT 
     NAME 
   , STATE 
+  , STATE_MESSAGE
   , REFRESH_START_TIME 
   , REFRESH_END_TIME 
   , TO_TIME(TO_TIMESTAMP(TIMESTAMPDIFF(second , REFRESH_START_TIME , REFRESH_END_TIME))) AS REFRESH_DURATION
 FROM 
   TABLE(information_schema.DYNAMIC_TABLE_REFRESH_HISTORY (
-      DATA_TIMESTAMP_START => TO_TIMESTAMP_LTZ('2024-04-15 14:27:00') 
+      DATA_TIMESTAMP_START => TO_TIMESTAMP_LTZ('2024-04-15 14:00:00')
+     , RESULT_LIMIT => 100
      , ERROR_ONLY => FALSE
   )
-);
+)
+ORDER BY  NAME 
+         ,REFRESH_DURATION DESC;
 
 ------------------------------------------------------------------------------------
 --■■■■■■■■■■■■■■■■■■■■■■■■■■■ 2. DYNAMIC_TABLE_GRAPH_HISTORY ■■■■■■■■■■■■■■■■■■■■■■■■■■■■
@@ -74,3 +81,12 @@ SELECT to_time(VALID_TO), to_time(VALID_FROM)
     HISTORY_START => DATEADD(MINUTES, -10, CURRENT_TIMESTAMP())
   )
 );
+
+
+
+
+
+
+
+
+
